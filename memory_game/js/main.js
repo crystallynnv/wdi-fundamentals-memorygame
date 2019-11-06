59402aa31884c95cfec419531b044e4cfea9c60a
Home <home@homes-mbp.lan>
2019-11-06 10:44:16 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -28,26 +28,44 @@ var cardsInPlay = [];
 
 
 function checkForMatch() {
-
-	if (cardsInPlay.length === 2) {
+	
+	
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
 		alert("Sorry, try again");
-	}}
-}
+	}};
 
-function flipCard(cardId) {
 
-	
+function flipCard() {
+
+	var cardId = this.getAttribute('data-id')
 	cardsInPlay.push(cards[cardId].rank);
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+	this.setAttribute('src',cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+
 
 checkForMatch();
-
-
 }
-flipCard(0);
-flipCard(2);
+};
+
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src','images/back.png');
+		cardElement.setAttribute('data-id',i);
+		cardElement.addEventListener('click',flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+createBoard();
+
+var resetBoard = function() {
+	window.location.reload();
+};
+var button = document.getElementById('reset');
+button.addEventListener('click',resetBoard);
